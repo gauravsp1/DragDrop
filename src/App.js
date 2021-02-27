064057fe3f551ff54da26aae59c12fe6d540a22f
Gaurav Pawar <gauravspawar9624@gmail.com>
2021-02-27 20:14:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import DragDrop from "./DragDrop"
+
 
 function App() {
+
+  const data = [{ title: "Task", items: ["Buy Milk", "Grocery", "Iron Clothes"] },
+  { title: "To-Do", items: ["Buy Water", "Walk", "Wash Clothes"] },
+  { title: "Done", items: ["Buy Juice", "Run", "Fold Clothes"] }]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDrop data={data}/>
+
+
     </div>
   );
 }
---
+++ src/DragDrop.js
@@ -0,0 +1,76 @@
+import React, {useState, useRef } from "react";
+
+function DragDrop(props){
+
+    const [data, setData] = useState(props.data); 
+    const [dragging, setDragging] = useState(false);
+
+    const dragItem = useRef();
+    const dragItemNode = useRef();
+
+     function handletDragStart(e, item){
+         
+
+        dragItemNode.current = e.target;
+        dragItemNode.current.addEventListener('dragend', handleDragEnd)
+        dragItem.current = item;
+
+        setTimeout(() => {
+            setDragging(true); 
+        },0)       
+    }
+
+    function handleDragEnd(){
+        setDragging(false);
+        dragItem.current = null;
+        dragItemNode.current.removeEventListener('dragend', handleDragEnd)
+        dragItemNode.current = null;
+    }
+    
+    function handleDragEnter(e, targetItem){
+        
+        if (dragItemNode.current !== e.target) {
+          console.log(targetItem.itemId);
+            setData((prev) => {
+                let newList = [...prev]
+               newList[targetItem.groupId].items.splice(targetItem.itemId, 0, newList[dragItem.current.groupId].items.splice(dragItem.current.itemId,1))
+                dragItem.current = targetItem;
+                return newList
+            })
+        }
+    }
+
+
+
+
+    function getEffect(item){
+        if (dragItem.current.groupId === item.groupId && dragItem.current.itemId === item.itemId) {
+            return "dnd_item current"
+        }
+        return "dnd_item"
+    }
+
+
+
+    return <div className="dragdrop">
+    {data.map((group,groupId)=>{
+      return <div 
+      onDragEnter={dragging && !group.items.length?(e) => handleDragEnter(e,{groupId, itemId: 0}):null}
+      className="dnd_group">
+       <div className="dnd_group_title">
+       {group.title}
+       </div>
+        
+        {group.items.map((item,itemId)=>{
+          return <div draggable onDragStart={(e) => handletDragStart(e, {groupId, itemId})} 
+          onDragEnter={dragging?(e) => {handleDragEnter(e, {groupId, itemId})}:null}
+            className={dragging?getEffect({groupId, itemId}):"dnd_item"}>
+            {item}
+          </div>
+        })}
+      </div>
+    })}
+  </div>
+}
+
+export default DragDrop
